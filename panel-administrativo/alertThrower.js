c8517620817b54627ff9a35b262e9a71da85fa63
trescuerpos <kevin.navarrogrz@gmail.com>
2026-09-09 16:52:24 -0600

    Agregada la logica y el modelo para agregar producto

--- panel-administrativo/alertThrower.js
+++ panel-administrativo/alertThrower.js
@@ -1,7 +1,7 @@
 export class AlertThrower {
     isSuccessful;
-    successAlert = `<div class="alert alert-success" role="alert">Producto guardado correctamente!</div>`;
-    errorAlert = `<div class="alert alert-warning" role="alert">El producto no puede ser guardado!</div>`;
+    successAlert = `<div class="alert alert-success" role="alert" tabindex="-1">Producto guardado correctamente!</div>`;
+    errorAlert = `<div class="alert alert-warning" role="alert" tabindex="-1">El producto no puede ser guardado!</div>`;
 
     constructor(operationResult) {
         this.isSuccessful = operationResult;
@@ -10,6 +10,13 @@ export class AlertThrower {
     throwAlert() {
         let target = document.getElementById("liveAlertPlaceholder");
         target.innerHTML = this.isSuccessful ? this.successAlert : this.errorAlert;
+
+        target.scrollIntoView({
+            behavior: "smooth",
+            block: "center"
+        });
+
+        target.focus();
     }
 
 
